1abba6689b23c34aa30e825767cde9ec5f292e60
Manol Donev <manol.donev@gmail.com>
2022-09-12 21:39:56 +0300

    feat: add box selection support

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { BoxList } from '../components/BoxList';
+import { Box } from '../components/Box';
 import { getShapes } from '../models/shape';
 
 const App = (): JSX.Element => {
@@ -7,7 +7,14 @@ const App = (): JSX.Element => {
   return (
     <main>
       <h1 className="m-4">Visualization assignment</h1>
-      <BoxList items={shapes} className="m-4" />
+
+      <div className="m-4">
+        <div className=" mx-auto flex max-w-5xl flex-wrap gap-x-2 gap-y-3">
+          {shapes.map((item) => (
+            <Box key={item.id} data={item} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 };
