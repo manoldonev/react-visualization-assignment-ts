2fffb17b2dc6d2d6fb74279c56ada0ff776fa3b1
Manol Donev <manol.donev@gmail.com>
2022-09-19 15:39:46 +0300

    chore: naming typo

--- src/routes/dashboard/UndoButton/UndoButton.tsx
+++ src/routes/dashboard/UndoButton/UndoButton.tsx
@@ -3,14 +3,14 @@ import { undoStoreAtom, undoAtom } from '../atoms';
 
 const UndoButton = ({ className = '' }: { className?: string }): JSX.Element => {
   const [undoLog] = useAtom(undoStoreAtom);
-  const [, setUndoAtom] = useAtom(undoAtom);
+  const [, setUndo] = useAtom(undoAtom);
 
   return (
     <button
       type="button"
       className={`h-12 w-24 rounded bg-blue-500 py-2 px-4 text-sm font-medium uppercase text-white enabled:hover:bg-blue-700 disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
       disabled={undoLog.length === 0}
-      onClick={setUndoAtom}
+      onClick={setUndo}
     >
       Undo
     </button>
